0dd66094439f5c4d4cd713d159e2c8717dd70387
randomusert <eetupajamaki@gmail.com>
2026-04-18 13:03:12 +0300

    Prepare for development of Start forge block entity

--- src/main/java/io/github/randomusert/mods/ogtts_core/Ogtts_core.java
+++ src/main/java/io/github/randomusert/mods/ogtts_core/Ogtts_core.java
@@ -84,7 +84,7 @@ public class Ogtts_core {
     }
 
     // You can use EventBusSubscriber to automatically register all static methods in the class annotated with @SubscribeEvent
-    @EventBusSubscriber(modid = MODID, bus = EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
+    @EventBusSubscriber(modid = MODID, value = Dist.CLIENT)
     public static class ClientModEvents {
         @SubscribeEvent
         public static void onClientSetup(FMLClientSetupEvent event) {
